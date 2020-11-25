881ea723bfbdf2a977d7bc9eff7093e36b344b54
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-11-26 04:59:55 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect  } from 'react';
+import './App.css';
+import {getQuizDetails} from "./services/quiz_service";
+function App() {
+  
+useEffect(()=> {
+  
+getQuizDetails(5, 'easy');
+
+}, []);
+
+
+
+  return (
+    <div className="App">
+      
+    </div>
+
+  );
+}
+
+export default App;
