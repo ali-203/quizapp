14267f289083b6966e7b6f3d58d7531ca70bafb9
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-11-26 06:11:29 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect  } from 'react';
 import './App.css';
 import {getQuizDetails} from "./services/quiz_service";
+import {Quiz} from './Types/quiz_types';
+
 function App() {
+  let [quiz, setQuiz] = useState<>([])
   
 useEffect(()=> {
   async function fetchData(){
     const questions = await getQuizDetails(10, 'easy');
-  
 console.log(questions);
   }
   fetchData();
