05ce756dced87f90daa74046a3a1c2c6b4587346
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-11-26 05:51:58 +0500

    update

--- src/services/quiz_service.ts
+++ src/services/quiz_service.ts
@@ -2,7 +2,14 @@ import { Quiz, QuestionType } from  "./../Types/quiz_types";
 
 export const getQuizDetails = async(totalQuetions: number, level: string): Promise<Quiz[]> => {
 const res = await fetch(`https://opentdb.com/api.php?amount=${totalQuetions}&difficulty=${level}&type=multiple`)
-let data:QuestionType[] =  await res.json();
-const results = data.res
+
+let {results } =  await res.json();
+
+const quiz = results.map ((questionObj: Quiz, ind: number) => {
+return {
+    
+}
+}
+)
 //return results;
 }
