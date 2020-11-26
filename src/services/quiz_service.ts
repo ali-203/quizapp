801255a21ad242c60dc245acd3ccdfa5a0256e61
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-11-26 22:32:55 +0500

    update

--- src/services/quiz_service.ts
+++ src/services/quiz_service.ts
@@ -1,16 +1,19 @@
 
-import { Quiz, } from  "./../Types/quiz_types"; 
+import { Quiz } from  "./../Types/quiz_types"; 
+const shuffleArray = (array: any[]) =>
+
+[...array].sort(() => Math.random() - 0.5)
 
 export const getQuizDetails = async(totalQuetions: number, level: string): Promise<Quiz[]> => {
 const res = await fetch(`https://opentdb.com/api.php?amount=${totalQuetions}&difficulty=${level}&type=multiple`)
 
-let {results } =  await res.json();
+let { results } =  await res.json();
 
-const quiz = results.map ((questionObj: Quiz, ind: number) => {
+const quiz = results.map ((questionObj: Quiz) => {
 return {
 question: questionObj.question,
 answer: questionObj.correct_answer,
-Option: questionObj.incorrect_answers.concat(questionObj.correct_answer)
+Option: shuffleArray(questionObj.incorrect_answers.concat(questionObj.correct_answer))
 
 }
 }
